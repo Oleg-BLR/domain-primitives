34333f3c585d8b3878473edbe75ee72c96398174
Oleg <Oleg-BLR@yandex.ru>
2023-03-27 00:17:16 +0300

    second commit

--- src/main/java/com/beginsecure/domain/primitives/SSN.java
+++ src/main/java/com/beginsecure/domain/primitives/SSN.java
@@ -1,2 +1,20 @@
-package com.beginsecure.domain.primitives;public class SSN {
+package com.beginsecure.domain.primitives;
+
+import static org.apache.commons.lang3.BooleanUtils.isTrue;
+import static org.apache.commons.lang3.Validate.inclusiveBetween;
+import static org.apache.commons.lang3.Validate.notNull;
+
+public class SSN {
+
+    private final String ssn;
+
+    public SSN(String ssn) {
+
+        notNull(ssn);
+        inclusiveBetween(11, 11, ssn.length());
+        isTrue(ssn.matches("^(?!666|000|9\\d{2})\\d{3}"
+                + "-(?!00)\\d{2}"
+                + "-(?!0{4})\\d{4}$"));
+        this.ssn = ssn;
+    }
 }
